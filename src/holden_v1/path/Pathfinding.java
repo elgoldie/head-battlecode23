2393e1ab3212bcfb57aa6fcf6a1b8782acb4ba38
5space <holdenew@gmail.com>
2023-01-11 17:11:09 -0500

    refactoring + changing pathfinding hierarchy

--- src/holden_v1/path/Pathfinding.java
+++ src/holden_v1/path/Pathfinding.java
@@ -1,16 +1,9 @@
 package holden_v1.path;
 
-import battlecode.common.*;
+import battlecode.common.Direction;
+import battlecode.common.MapLocation;
 
-public class Pathfinding {
+public interface Pathfinding {
 
-    public RobotController rc;
-    public int width;
-    public int height;
-    
-    public Pathfinding(RobotController rc, int width, int height) {
-        this.rc = rc;
-        this.width = width;
-        this.height = height;
-    }
+    public Direction findPath(MapLocation target);
 }
