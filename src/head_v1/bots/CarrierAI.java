8589fba4287c31317497ea0a969cbe87c4b23893
elgoldie <elgoldie@mit.edu>
2023-01-11 16:28:52 -0500

    carriers pick up anchor

--- src/head_v1/bots/CarrierAI.java
+++ src/head_v1/bots/CarrierAI.java
@@ -1,6 +1,9 @@
 package head_v1.bots;
 
 import battlecode.common.*;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Arrays;
 
 public class CarrierAI extends RobotAI {
 
@@ -20,29 +23,40 @@ public class CarrierAI extends RobotAI {
     public void run() throws GameActionException {
         super.run();
         
-        // if (rc.getAnchor() != null) {
+        if (rc.getAnchor() != null) {
 
-        //     int[] islands = rc.senseNearbyIslands();
-        //     Set<MapLocation> islandLocs = new HashSet<>();
-        //     for (int id : islands) {
-        //         MapLocation[] thisIslandLocs = rc.senseNearbyIslandLocations(id);
-        //         islandLocs.addAll(Arrays.asList(thisIslandLocs));
-        //     }
-        //     if (islandLocs.size() > 0) {
-        //         MapLocation islandLocation = islandLocs.iterator().next();
-        //         rc.setIndicatorString("Moving my anchor towards " + islandLocation);
-        //         while (!rc.getLocation().equals(islandLocation)) {
-        //             Direction dir = rc.getLocation().directionTo(islandLocation);
-        //             if (rc.canMove(dir)) {
-        //                 rc.move(dir);
-        //             }
-        //         }
-        //         if (rc.canPlaceAnchor()) {
-        //             rc.setIndicatorString("Huzzah, placed anchor!");
-        //             rc.placeAnchor();
-        //         }
-        //     }
-        // }
+            int[] islands = rc.senseNearbyIslands();
+            Set<MapLocation> islandLocs = new HashSet<>();
+            for (int id : islands) {
+                MapLocation[] thisIslandLocs = rc.senseNearbyIslandLocations(id);
+                islandLocs.addAll(Arrays.asList(thisIslandLocs));
+            }
+            if (islandLocs.size() > 0) {
+                MapLocation islandLocation = islandLocs.iterator().next();
+                rc.setIndicatorString("Moving my anchor towards " + islandLocation);
+                while (!rc.getLocation().equals(islandLocation)) {
+                    Direction dir = rc.getLocation().directionTo(islandLocation);
+                    if (rc.canMove(dir)) {
+                        rc.move(dir);
+                    }
+                }
+                if (rc.canPlaceAnchor()) {
+                    rc.setIndicatorString("Huzzah, placed anchor!");
+                    rc.placeAnchor();
+                }
+            }
+        }
+        // picks up anchor if possible
+        for (Direction dir : directions) {
+            MapLocation newLocation = rc.adjacentLocation(dir);
+            if (rc.canTakeAnchor(newLocation, Anchor.STANDARD)) {
+                rc.takeAnchor(newLocation, Anchor.STANDARD);
+                break;
+            }
+        
+
+
+        }
 
         if (getInventoryWeight() == 40) {
 
