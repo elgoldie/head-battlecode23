cb935aff9774990d9bcafe57bfc90e31abefafc4
Alpha-Kittens <90939705+Alpha-Kittens@users.noreply.github.com>
2023-01-11 21:39:01 -0500

    removing comment

--- src/macrobot/bots/HeadquartersAI.java
+++ src/macrobot/bots/HeadquartersAI.java
@@ -45,41 +45,6 @@ public class HeadquartersAI extends RobotAI {
         }
         
         
-        /*
-        if (gameTurn == 1) {
-
-            comm.appendLocation(0, rc.getLocation());
-            comm.writeInt(6, 6153);
-            System.out.println("HQ has written the byte to index 6");
-            rc.buildRobot(RobotType.CARRIER, rc.getLocation().add(directions[0]));
-            System.out.println("Robot spawned");
-        } else if (gameTurn == 2) {
-            this.hqLocations = comm.readLocationArray(0);
-            for (MapLocation loc : hqLocations) {
-                System.out.println(loc);
-            }
-        }
-        
-        Direction dir = directions[rng.nextInt(directions.length)];
-        MapLocation newLoc = rc.getLocation().add(dir);
-        if ((rc.canBuildAnchor(Anchor.STANDARD)) && (rc.getRobotCount()>30)) {
-            // If we can build an anchor do it!
-            rc.buildAnchor(Anchor.STANDARD);
-            rc.setIndicatorString("Building anchor! " + rc.getAnchor());
-        }
-        if (rng.nextBoolean()) {
-            // Let's try to build a carrier.
-            rc.setIndicatorString("Trying to build a carrier");
-            if (rc.canBuildRobot(RobotType.CARRIER, newLoc)) {
-                rc.buildRobot(RobotType.CARRIER, newLoc);
-            }
-        } else {
-            // Let's try to build a launcher.
-            rc.setIndicatorString("Trying to build a launcher");
-            if (rc.canBuildRobot(RobotType.LAUNCHER, newLoc)) {
-                rc.buildRobot(RobotType.LAUNCHER, newLoc);
-            }
-        }
-        */
+       
     }
 }
