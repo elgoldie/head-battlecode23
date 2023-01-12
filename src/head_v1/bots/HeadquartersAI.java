facb56150142d6a3322b33cf8428a36762517e9c
elgoldie <elgoldie@mit.edu>
2023-01-11 23:45:32 -0500

    headquarters Attempt 1

--- src/head_v1/bots/HeadquartersAI.java
+++ src/head_v1/bots/HeadquartersAI.java
@@ -7,8 +7,8 @@ public class HeadquartersAI extends RobotAI {
     public WellInfo startingAdamantium = null;
     public WellInfo startingMana = null;
 
-    MapLocation optimalSpawnAdamantium = null;
-    MapLocation optimalSpawnMana = null;
+    public MapLocation optimalSpawnAdamantium = null;
+    public MapLocation optimalSpawnMana = null;
     
     public HeadquartersAI(RobotController rc, int id) throws GameActionException {
         super(rc, id);
@@ -22,27 +22,63 @@ public class HeadquartersAI extends RobotAI {
         if (rc.getRoundNum() == 1) {
             comm.appendLocation(0, rc.getLocation());
             // finding closest starting wells
-            WellInfo[] wells = rc.senseNearbyWells();
-            int bestDistanceAdamantium = Integer.MAX_VALUE;
-            int bestDistanceMana = Integer.MAX_VALUE;
-            if (wells != null) {
-                for (WellInfo well : wells) {
-                    int distance = rc.getLocation().distanceSquaredTo(well.getMapLocation());
-                    if (well.getResourceType() == ResourceType.ADAMANTIUM) {
-                        if (distance < bestDistanceAdamantium) {
-                            bestDistanceAdamantium = distance;
-                            startingAdamantium = well;
-                        }
-                    } else {
-                        if (distance < bestDistanceMana) {
-                            bestDistanceMana = distance;
-                            startingMana = well;
-                        }
-                    }
-                }
+            // WellInfo[] wells = rc.senseNearbyWells();
+            // int bestDistanceAdamantium = Integer.MAX_VALUE;
+            // int bestDistanceMana = Integer.MAX_VALUE;
+            // if (wells != null) {
+            //     for (WellInfo well : wells) {
+            //         int distance = rc.getLocation().distanceSquaredTo(well.getMapLocation());
+            //         if (well.getResourceType() == ResourceType.ADAMANTIUM) {
+            //             if (distance < bestDistanceAdamantium) {
+            //                 bestDistanceAdamantium = distance;
+            //                 startingAdamantium = well;
+            //             }
+            //         } else {
+            //             if (distance < bestDistanceMana) {
+            //                 bestDistanceMana = distance;
+            //                 startingMana = well;
+            //             }
+            //         }
+            //     }
     
-                // finding optimal spawn locations
-            }
+            //     // finding optimal spawn locations
+
+            //     // find closest square to optimal spawn location
+            //     if (startingAdamantium != null) {
+            //         Direction dirAdamantium = startingAdamantium.getMapLocation().directionTo(rc.getLocation());
+            //         MapLocation attemptedSpawnAdamantium = startingAdamantium.getMapLocation();
+            //         while (optimalSpawnAdamantium == null) {
+            //             //System.out.println("Adamantium Spawn search attempted");
+            //             if (attemptedSpawnAdamantium.isWithinDistanceSquared(rc.getLocation(), 9)) {
+            //                 optimalSpawnAdamantium = attemptedSpawnAdamantium;
+            //                 System.out.println("Adamantium Spawn search complete");
+            //             } else {
+            //                 attemptedSpawnAdamantium.subtract(dirAdamantium);
+            //             }
+                        
+
+                        
+            //         }
+            //     }
+            //     if (startingMana != null) {
+            //         Direction dirMana = startingMana.getMapLocation().directionTo(rc.getLocation());
+            //         MapLocation attemptedSpawnMana = startingMana.getMapLocation();
+            //         while (optimalSpawnMana == null) {
+            //             //System.out.println("Mana Spawn search attempted");
+            //             if (attemptedSpawnMana.isWithinDistanceSquared(rc.getLocation(), 9)) {
+            //                 optimalSpawnMana = attemptedSpawnMana;
+            //                 System.out.println("Mana Spawn search complete");
+            //             } else {
+            //                 attemptedSpawnMana.subtract(dirMana);
+            //             }  
+            //         }    
+            //     }   
+            //     // spawn accordingly
+            //     if (rc.canBuildRobot(RobotType.CARRIER, optimalSpawnAdamantium)) {
+            //         rc.buildRobot(RobotType.CARRIER, optimalSpawnAdamantium);
+            //     }
+
+            // }
             
 
 
@@ -52,6 +88,23 @@ public class HeadquartersAI extends RobotAI {
             for (MapLocation loc : hqLocations) {
                 System.out.println(loc);
             }
+        //     if (rc.canBuildRobot(RobotType.CARRIER, optimalSpawnAdamantium)) {
+        //         rc.buildRobot(RobotType.CARRIER, optimalSpawnAdamantium);
+        //     }
+        // } else if (rc.getRoundNum() == 3) {
+        //     if (rc.canBuildRobot(RobotType.CARRIER, optimalSpawnAdamantium)) {
+        //         rc.buildRobot(RobotType.CARRIER, optimalSpawnAdamantium);
+        //     }
+        // } else if (rc.getRoundNum() == 4) {
+        //     if (startingMana != null) {
+        //         if (rc.canBuildRobot(RobotType.CARRIER, optimalSpawnMana)) {
+        //             rc.buildRobot(RobotType.CARRIER, optimalSpawnMana);
+        //         }
+        //     }
+        // } else if (rc.getRoundNum() <= 7) {
+        //     if (rc.canBuildRobot(RobotType.LAUNCHER, rc.getLocation().add(Direction.NORTH))) {
+        //         rc.buildRobot(RobotType.LAUNCHER, rc.getLocation().add(Direction.NORTH));
+        //     }
         }
 
 
